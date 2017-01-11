1d4ba768a979561e6fb2e23d4e446f4a23f1a740
“lukemccrae” <lukemccrae@gmail.com>
2017-01-11 15:13:06 -0700

    ready to heroku

--- migrations/20170111103943_book.js
+++ migrations/20170111103943_book.js
@@ -3,30 +3,6 @@ exports.up = function(knex, Promise) {
         table.increments();
         table.text('title').notNullable();
         table.text('genre');
-        table.integer('author_1_first_name')
-            .references('author.id')
-            .unsigned()
-            .notNullable();
-        table.integer('author_1_last_name')
-            .references('author.id')
-            .unsigned()
-            .notNullable();
-        table.integer('author_2_first_name')
-            .references('author.id')
-            .unsigned()
-            .notNullable();
-        table.integer('author_2_last_name')
-            .references('author.id')
-            .unsigned()
-            .notNullable();
-        table.integer('author_3_first_name')
-            .references('author.id')
-            .unsigned()
-            .notNullable();
-        table.integer('author_3_last_name')
-            .references('author.id')
-            .unsigned()
-            .notNullable();
         table.text('description');
         table.text('cover_url');
     })
