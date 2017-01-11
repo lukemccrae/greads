1d1a34b6fe57387750a809354a3df161e9c7c7bd
“lukemccrae” <lukemccrae@gmail.com>
2017-01-11 15:14:43 -0700

    quick change

--- myapp/app.js
+++ myapp/app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+var knex = require('./db_connections.js')
 var app = express();
 
 // view engine setup
