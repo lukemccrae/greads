33a13d6a740d76c9041781cfa87c9baf7b1dc1f9
“lukemccrae” <lukemccrae@gmail.com>
2017-01-12 13:32:25 -0700

    delete works

--- server/app.js
+++ server/app.js
@@ -5,10 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var knex = require('../db/knex')
+var methodOverride = require('method-override')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var book = require('./routes/book')
+var book = require('./routes/book');
 
 var app = express();
 
@@ -20,29 +21,32 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(methodOverride('_method'))
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/book', book);
 app.use('/users', users);
-app.use('/book', book)
+app.use('/', book)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('you seem lost');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- server/routes/book.js
+++ server/routes/book.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../../db/knex')
+var methodOverride = require('method-override')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,18 +13,49 @@ router.get('/', function(req, res, next) {
                 title: title
             });
         });
-})
+});
 
 router.get('/add', function(req, res, next) {
-    knex('book')
-        .select()
-        .then(title => {
-            console.log(title);
-            res.render('add_books', {
-                title: title
-            });
-        });
+    res.render('add_books')
+});
+
+router.get('/delete', function(req, res, next) {
+    res.render('delete_book')
+});
+
+router.get('/delete/:id', function(req, res, next) {
+    res.render('delete_book')
+});
+
+router.post('/add', function(req, res, next) {
+    console.log('hi');
+    console.log(req.body);
+    const newBook = {
+        title: req.body.title,
+        genre: req.body.genre,
+        description: req.body.description,
+        cover_url: req.body.cover
+    }
+    if (newBook.title) {
+        knex('book')
+            .insert(newBook)
+            .then(res.redirect('/'))
+    } else {
+        res.render('error', {
+            message: 'no title'
+        })
+    }
 })
+
+router.delete('/delete/:id', (req, res) => {
+    knex('book')
+        .where('id', req.params.id)
+        .del()
+        .then(res.render('confirm_delete', {
+            message: 'book deleted'
+        }))
+});
+
 
 
 module.exports = router;
